bd88e5c81b956988da1aa30c12e454a06ac581e6
Quirin Brändli <quirin.braendli@gmx.de>
2020-02-04 21:57:36 +0100

    Some Changes for exceptions and code cleanup

--- src/main/java/de/kelanisystem/kelaniconfig/KelaniConfigManager.java
+++ src/main/java/de/kelanisystem/kelaniconfig/KelaniConfigManager.java
@@ -6,73 +6,66 @@ import org.simpleyaml.exceptions.InvalidConfigurationException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class KelaniConfigManager {
-        //PushTest
-        private Map<String, YamlFile> configs;
-        private File configFolder = new File("data/");
+    //PushTest
+    private Map<String, YamlFile> configs;
+    private File configFolder = new File("data/");
 
-        public KelaniConfigManager(String... configs) {
-            this.configs = new HashMap<>();
+    public KelaniConfigManager() {
+        this.configs = new HashMap<>();
+    }
 
-            createFile(configs);
+    public KelaniConfigManager(File configFolder) {
+        this.configFolder = configFolder;
+    }
+
+    public List<YamlConfiguration> createFile(String... name) throws IOException, InvalidConfigurationException {
+        return createFile(configFolder, name);
+    }
+
+    public List<YamlConfiguration> createFile(File path, String... names) throws IOException, InvalidConfigurationException {
+        List<YamlConfiguration> configurations = new ArrayList<>();
+
+        if (!path.exists())
+            if (!configFolder.mkdir()) throw new IOException("Could not create " + configFolder.getName());
+
+        for (String name : names) {
+            YamlFile yamlFile = new YamlFile(path + "/" + name + ".yml");
+
+            if (!yamlFile.exists())
+                yamlFile.createNewFile(true);
+
+            yamlFile.load();
+
+
+            configs.put(name, yamlFile);
+            configurations.add(yamlFile);
         }
 
-        public KelaniConfigManager(File configFolder, String... configs){
-            this(configs);
-            this.configFolder = configFolder;
-        }
-
-        public void createFile(String... name) {
-            createFile(configFolder, name);
-        }
-
-        public void createFile(File path, String... names ) {
-            if(!path.exists())
-                configFolder.mkdir();
-
-            for(String name : names) {
-                YamlFile yamlFile = new YamlFile(path + "/" + name +".yml");
-
-                try {
-                    if(!yamlFile.exists()){
-                        System.out.println("Creating new Configuration: " + name + ".yml");
-                        yamlFile.createNewFile(true);
-                    }
-                    else
-                        System.out.println("Loading new Configuration: " + name + ".yml");
-                    yamlFile.load();
-
-                } catch (InvalidConfigurationException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                configs.put(name, yamlFile);
-            }
-        }
+        return configurations;
+    }
 
     public YamlConfiguration getConfig(String config) {
         return configs.get(config);
     }
 
-        public void copyDefaults(String config, boolean bool) {
-            this.configs.get(config).options().copyDefaults(bool);
+    public void copyDefaults(String config, boolean bool) throws IOException {
+        this.configs.get(config).options().copyDefaults(bool);
 
-            save(config);
-        }
+        save(config);
+    }
 
-        public void addDefault(String config, String key, Object value) {
-            this.configs.get(config).addDefault(key, value);
-            System.out.println("added Default to " + config + " | " + key + " | " + value);
+    public void addDefault(String config, String key, Object value) {
+        this.configs.get(config).addDefault(key, value);
 
-        }
+    }
 
-        public void addDefaults(String config, Map<String, Object> defaults) {
+    public void addDefaults(String config, Map<String, Object> defaults) {
             this.configs.get(config).addDefaults(defaults);
         }
 
@@ -92,27 +85,25 @@ public class KelaniConfigManager {
             return this.configs.get(config).getInt(key);
         }
 
-        public List<String> getStringList(String config, String key) {
-            return this.configs.get(config).getStringList(key);
-        }
-        public List<Integer> getIntegerList(String config, String key) {
-            return this.configs.get(config).getIntegerList(key);
-        }
-
-        public List<?> getList(String config, String key) {
-            return this.configs.get(config).getList(key);
-        }
-
-
-        public void setHeader(String config, String header) {
-            this.configs.get(config).options().header(header);
-            save(config);
-        }
-        public void save(String config) {
-            try {
-                this.configs.get(config).save();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    public List<String> getStringList(String config, String key) {
+        return this.configs.get(config).getStringList(key);
     }
+
+    public List<Integer> getIntegerList(String config, String key) {
+        return this.configs.get(config).getIntegerList(key);
+    }
+
+    public List<?> getList(String config, String key) {
+        return this.configs.get(config).getList(key);
+    }
+
+
+    public void setHeader(String config, String header) throws IOException {
+        this.configs.get(config).options().header(header);
+        save(config);
+    }
+
+    public void save(String config) throws IOException {
+        this.configs.get(config).save();
+    }
+}
